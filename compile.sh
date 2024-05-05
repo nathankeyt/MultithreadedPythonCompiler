67b3e10bce3c9cf7b65ce702d2dedbd64d3a30a6
Nathan <nake8894@colorado.edu>
2024-05-05 05:11:07 +0000

    multiple file compiling working

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
-python3 compiler.py test.py
+python3 compiler.py test.py test2.py
+
+gcc -m32 -g test2.s runtime/libpyyruntime.a -lm -o test2
+
+./test2
 
 gcc -m32 -g test.s runtime/libpyyruntime.a -lm -o test
 
